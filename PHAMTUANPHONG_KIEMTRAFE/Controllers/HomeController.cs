275c99b0a5091c49435fa8408dc99392e0151b2d
ASUS Vivobook <ASUS Vivobook@LAPTOP-RA51567G>
2024-10-08 19:19:44 +0700

    Add project files.

---
+++ PHAMTUANPHONG_KIEMTRAFE/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PHAMTUANPHONG_KIEMTRAFE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult KiemTra()
+        {
+            return View();
+        }
+
+    }
+}
